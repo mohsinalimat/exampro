892f9d981241d56fdb6859d47cdbb40fd18b7b7a
Labeeb Mattra <labeeb@zerodha.com>
2024-12-04 13:34:09 +0530

    Add doctype skeltons

---
+++ exampro/exam_pro/doctype/exam_category_metrics/exam_category_metrics.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Labeeb Mattra and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Exam Category Metrics", {
+// 	refresh(frm) {
+
+// 	},
+// });
