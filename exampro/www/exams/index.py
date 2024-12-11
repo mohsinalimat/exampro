c34fdc5bfe2274cc9497fc3317c213f4f24f8e7e
Labeeb Mattra <labeeb@zerodha.com>
2024-12-11 11:55:51 +0530

    Delete redundant code and files

--- exampro/www/exams/index.py
+++ exampro/www/exams/index.py
@@ -1,14 +1,125 @@
+from datetime import datetime, timedelta
 import frappe
+from frappe.utils import now
 from frappe import _
 from exampro.exam_pro.utils import (
-	# Exams and Course ceators share same role
-	can_create_courses,
 	check_profile_restriction,
-	get_restriction_details,
-	has_course_moderator_role
+	get_restriction_details
 )
-from lms.overrides.user import get_registered_exams, get_authored_exams, get_live_exam
 
+def get_live_exam(member=None):
+	"""
+	Get upcoming/ongoing exam of a candidate.
+
+	Check if current time is inbetween start and end time
+	Function returns only one live/upcoming exam details
+	even if multiple entries are there.
+	"""
+	exam_details = {}
+
+	submissions = frappe.get_all(
+		"Exam Submission",
+		{"candidate": member or frappe.session.user},[
+			"name",
+			"exam_schedule",
+			"status",
+			"exam_started_time",
+			"exam_submitted_time",
+			"additional_time_given"
+   ])
+	for submission in submissions:
+		if submission["status"] in ["Registration Cancelled", "Aborted"]:
+			continue
+
+		schedule = frappe.get_doc("Exam Schedule", submission["exam_schedule"])
+		exam = frappe.get_doc("Exam", schedule.exam)
+
+		# end time is schedule start time + duration + additional time given
+		end_time = schedule.start_date_time + timedelta(minutes=schedule.duration) + \
+			timedelta(minutes=submission["additional_time_given"])
+
+		exam_details = {
+			"exam_submission": submission["name"],
+			"exam": schedule.exam,
+			"exam_schedule": submission["exam_schedule"],
+			"start_time": schedule.start_date_time,
+			"end_time": end_time,
+			"additional_time_given": submission["additional_time_given"],
+			"submission_status": submission["status"],
+			"duration": schedule.duration,
+			"enable_calculator": exam.enable_calculator,
+			"live_status": "",
+			"submission_status": submission["status"],
+			"enable_video_proctoring": exam.enable_video_proctoring,
+			"enable_chat": exam.enable_chat,
+			"enable_calculator": exam.enable_calculator
+		}
+
+		# make datetime in isoformat
+		for key,val in exam_details.items():
+			if type(val) == datetime:
+				exam_details[key] = val.isoformat()
+
+		# checks if current time is between schedule start and end time
+		# ongoing exams can be in Not staryed, started or submitted states
+		tnow = datetime.strptime(now(), '%Y-%m-%d %H:%M:%S.%f')
+		if schedule.start_date_time <= tnow <= end_time:
+			exam_details["live_status"] = "Live"
+			return exam_details
+		elif tnow <= schedule.start_date_time:
+			exam_details["live_status"] = "Upcoming"
+			return exam_details
+		elif tnow > end_time:
+			# if time is over, submit if applicable
+			if submission["status"] != "Submitted":
+				doc = frappe.get_doc("Exam Submission", submission["name"])
+				doc.status == "Submitted"
+				doc.save(ignore_permissions=True)
+
+			return {}
+
+	return exam_details
+
+
+
+def get_exam_registation(candidate=None):
+	"""Returns all exam registrations of the user."""
+
+	filters = {"candidate": candidate or frappe.session.user}
+	return frappe.get_all("Exam Submission", filters, ["name", "exam", "status"])
+
+
+def get_registered_exams():
+	in_progress = []
+	completed = []
+	memberships = get_exam_registation(None)
+
+	for membership in memberships:
+		exam = frappe.db.get_value(
+			"Exam",
+			membership.exam,
+			[
+				"name",
+				"upcoming",
+				"title",
+				"image",
+				"enable_certification",
+				"paid_certificate",
+				"price_certificate",
+				"currency",
+				"published",
+			],
+			as_dict=True,
+		)
+		if not exam.published:
+			continue
+		progress = membership.status
+		if progress == "Submitted":
+			completed.append(exam)
+		else:
+			in_progress.append(exam)
+
+	return {"in_progress": in_progress, "completed": completed}
 
 def get_context(context):
 	"""
@@ -27,15 +138,7 @@ def get_context(context):
 	context.enrolled_exams = (
 		get_registered_exams()["in_progress"] + get_registered_exams()["completed"]
 	)
-	context.created_courses = get_authored_exams(None, False)
-	context.restriction = check_profile_restriction()
-	context.show_creators_section = can_create_courses()
-	context.show_review_section = (
-		has_course_moderator_role() and frappe.session.user != "Guest"
-	)
-
-	if context.restriction:
-		context.restriction_details = get_restriction_details()
+	context.created_exams = []
 
 	context.metatags = {
 		"title": _("Exam List"),
@@ -61,10 +164,10 @@ def get_exams():
 		],
 	)
 
-	live_exams, upcoming_courses = [], []
+	live_exams, upcoming_exams = [], []
 	for exam in exams:
 		if exam.upcoming:
-			upcoming_courses.append(exam)
+			upcoming_exams.append(exam)
 		else:
 			live_exams.append(exam)
-	return live_exams, upcoming_courses
+	return live_exams, upcoming_exams
