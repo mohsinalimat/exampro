3bae050560b59df07275c5279250f6f8292c7347
Labeeb Mattra <labeeb@zerodha.com>
2025-06-26 15:25:27 +0530

    feat: Remove Exam Batch Submission functionality and related files

--- exampro/exam_pro/doctype/exam_batch_submission/exam_batch_submission.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Labeeb Mattra and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Exam Batch Submission", {
-// 	refresh(frm) {
-
-// 	},
-// });
