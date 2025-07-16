8df2cc91085920888f96aa85d86434b46f3a0071
Labeeb Mattra <labeeb@zerodha.com>
2025-07-16 19:43:04 +0530

    fix: Remove unused examiner validation and overlap check from ExamSchedule

--- exampro/exam_pro/doctype/exam_schedule/exam_schedule.py
+++ exampro/exam_pro/doctype/exam_schedule/exam_schedule.py
@@ -41,9 +41,6 @@ class ExamSchedule(Document):
 				"Warning: Exam with question type:{} needs evaluation. Add examiner list.".format(
 					self.question_type
 			))
-		
-		# validate examiner list
-		self.validate_examiner_list()
 
 		# validate cert template
 		# if self.certificate_template != "":
@@ -231,49 +228,6 @@ class ExamSchedule(Document):
 		 
 		return True
 
-	def validate_examiner_list(self):
-		"""
-		get all the other exams which falls in the current exam's timeframe
-		make sure that there is no conflicting proctor.
-		Conflicting evaluator is fine, since they are not time bound.
-		
-		Also checks if examiners have the appropriate roles based on their assigned flags,
-		and assigns those roles if needed.
-		"""
-		if not self.examiners:
-			return
-		
-		if not isinstance(self.start_date_time, datetime):
-			exam_start = parse(self.start_date_time)
-		else:
-			exam_start = self.start_date_time
-		end_time = exam_start + timedelta(minutes=self.duration)
-		other_exams = frappe.get_all(
-			"Exam Schedule",
-			filters=[["start_date_time", ">=", exam_start]],
-			fields=["name", "duration", "start_date_time"], 
-		)
-
-		for exam2 in other_exams:
-			if exam2["name"] == self.name:
-				continue
-
-			examiners1 = [ex.examiner for ex in self.examiners]
-			exam2_end = exam2["start_date_time"] + timedelta(minutes=exam2["duration"])
-			if check_overlap(exam_start, end_time, exam2["start_date_time"], exam2_end):
-				examiners2 = frappe.db.get_all(
-					"Examiner",
-					filters={"parent": self.name, "can_proctor": 1},
-					fields=["examiner"]
-				)
-				examiners2 = [ex.examiner for ex in examiners2]
-				# check if any examiner in the 2nd list
-				overlap = set(examiners1).intersection(set(examiners2))
-				if overlap:
-					frappe.throw(
-						"Can't add {} as proctor(s). Overlap found with schedule {}".format(
-							overlap, exam2["name"]
-						))
 			
 	def as_dict(self, no_nulls=False):
 		"""Convert the document to a dict for the list view, including the status"""
@@ -302,15 +256,6 @@ class ExamSchedule(Document):
 		self.save(ignore_permissions=True)
 		
 		return invite_link
-	
-def check_overlap(start_time1, end_time1, start_time2, end_time2):
-	assert isinstance(start_time1, datetime), "start_time1 must be a datetime object"
-	assert isinstance(end_time1, datetime), "end_time1 must be a datetime object"
-	assert isinstance(start_time2, datetime), "start_time2 must be a datetime object"
-	assert isinstance(end_time2, datetime), "end_time2 must be a datetime object"
-
-	# Check if Period 1 starts before Period 2 ends AND Period 1 ends after Period 2 starts
-	return start_time1 < end_time2 and end_time1 > start_time2
 
 
 def _submit_pending_exams(schedule_name):
